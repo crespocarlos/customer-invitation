bc220b423e926da7a6b49c557c4a6ab22d68f2f2
ccrespo-ciandt <ccrespo@ciandt.com>
2017-09-10 20:12:02 -0300

    task implementation; unit tests

---
+++ CustomerInvitation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CustomerInvitation.Service;
+using CustomerInvitation.Service.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerInvitation
+{
+    class Program
+    {
+        /// <summary>
+        /// Initialize the dependency injection container
+        /// </summary>
+        /// <returns>service providers</returns>
+        static IServiceProvider InitializeDIContainer() {
+            var services = new ServiceCollection();
+            services.AddTransient<IGeographicDistance, GeographicDistance>();
+            services.AddTransient<IFileReader, FileReader>();
+            services.AddTransient<IInvitation, Invitation>();
+            return services.BuildServiceProvider();
+        }
+
+        static void Main(string[] args)
+        {
+            var provider = InitializeDIContainer();
+            var party = provider.GetService<IInvitation>();
+            
+            party.ListCustomersToInviteWithinDistance("./Resource/customerList.json", 100);
+        }
+
+        
+    }
+}
